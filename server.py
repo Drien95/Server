3189679814ec675f34d63d9d230a38b553a71139
Adrien <80692395+Drien95@users.noreply.github.com>
2024-12-09 10:25:05 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 from http.server import SimpleHTTPRequestHandler, HTTPServer
 
 class LoggingHTTPRequestHandler(SimpleHTTPRequestHandler):
@@ -5,25 +6,26 @@ class LoggingHTTPRequestHandler(SimpleHTTPRequestHandler):
         print("\n--- New GET Request ---")
         print("Path:", self.path)
         print("Headers:\n", self.headers)
-        super().do_GET()  # Traitement standard
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"GET request received")
 
     def do_POST(self):
         print("\n--- New POST Request ---")
         print("Path:", self.path)
         print("Headers:\n", self.headers)
         
-        # Lire et afficher le corps de la requête POST
         content_length = int(self.headers.get('Content-Length', 0))
         post_data = self.rfile.read(content_length)
         print("Body:\n", post_data.decode('utf-8'))
         
-        # Réponse
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"POST request received")
 
-# Lancer le serveur sur le port 8080
-server_address = ('', 8080)
+# Utilise le port défini par Render
+PORT = int(os.environ.get('PORT', 8080))
+server_address = ('', PORT)
 httpd = HTTPServer(server_address, LoggingHTTPRequestHandler)
-print("Serving on port 8080...")
+print(f"Serving on port {PORT}...")
 httpd.serve_forever()
